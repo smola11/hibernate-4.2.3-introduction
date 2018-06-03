d8e0b30a456029d888d027f3cbb5eaee5ca72202
Maciej  Smolarz <smola11@o2.pl>
2018-06-03 11:13:07 +0200

    Connection check

--- src/main/java/com/maciej/Application.java
+++ src/main/java/com/maciej/Application.java
@@ -1,8 +1,14 @@
 package com.maciej;
 
+import org.hibernate.Session;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Session session = HibernateUtilities.getSessionFactory().openSession();
+        session.close();
+
     }
 }
