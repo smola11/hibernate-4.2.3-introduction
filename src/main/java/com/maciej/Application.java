7c22ea4aed9478b9f8b684f78d639cd2c9e0e406
Maciej  Smolarz <smola11@o2.pl>
2018-06-03 22:28:29 +0200

    Application edited

--- src/main/java/com/maciej/Application.java
+++ src/main/java/com/maciej/Application.java
@@ -6,10 +6,9 @@ import org.hibernate.Session;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
         Session session = HibernateUtilities.getSessionFactory().openSession();
 
+        // save User
         session.beginTransaction();
         User user = new User();
         user.setName("Joe");
@@ -17,8 +16,16 @@ public class Application {
         session.save(user);
         session.getTransaction().commit();
 
+        // get User
+        session.beginTransaction();
+        User loadedUser = (User) session.get(User.class, 1L);
+        System.out.println(loadedUser.getName());
+        System.out.println(loadedUser.getGoal());
+        // Hibernate will do automatic update of User after changing using proxy
+        loadedUser.setTotal(loadedUser.getTotal() + 50);
+        session.getTransaction().commit();
+
         session.close();
         HibernateUtilities.getSessionFactory().close();
-
     }
 }
