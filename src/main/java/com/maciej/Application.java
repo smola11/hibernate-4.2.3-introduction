1296eecbddfbf906432d6b61b6e7db3928d7f70b
Maciej  Smolarz <smola11@o2.pl>
2018-06-03 12:53:01 +0200

    Saving User object

--- src/main/java/com/maciej/Application.java
+++ src/main/java/com/maciej/Application.java
@@ -1,5 +1,6 @@
 package com.maciej;
 
+import com.maciej.entity.User;
 import org.hibernate.Session;
 
 public class Application {
@@ -8,7 +9,16 @@ public class Application {
         System.out.println("Hello World!");
 
         Session session = HibernateUtilities.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        User user = new User();
+        user.setName("Joe");
+        user.setGoal(250);
+        session.save(user);
+        session.getTransaction().commit();
+
         session.close();
+        HibernateUtilities.getSessionFactory().close();
 
     }
 }
