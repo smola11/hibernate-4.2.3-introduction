475b6736b6ec47e117a6fbd4ee1a8c8b12cd25ca
Smolarz <mjsz@gft.com>
2018-06-05 11:47:38 +0200

    Edited connector and url connection for different database

--- src/main/java/com/maciej/Application.java
+++ src/main/java/com/maciej/Application.java
@@ -11,37 +11,39 @@ import java.util.Date;
 public class Application {
 
     public static void main(String[] args) {
-        Session session = HibernateUtilities.getSessionFactory().openSession();
 
-        // save User
-        session.beginTransaction();
-        User user = new User();
-        user.setName("Joe");
-        user.addHistory(new UserHistory(new Date(), "Set name to Joe"));
-        user.getProteinData().setGoal(250);
-        user.addHistory(new UserHistory(new Date(), "Set the goal to 250"));
-        user.getGoalAlerts().add(new GoalAlert("Congratulations!"));
-        user.getGoalAlerts().add(new GoalAlert("You did it!"));
-        session.save(user);
-        session.getTransaction().commit();
+        PopulateSampleData();
 
-        // get User
-        session.beginTransaction();
-        User loadedUser = (User) session.get(User.class, 1L);
-        System.out.println(loadedUser.getName());
-        System.out.println(loadedUser.getProteinData().getGoal());
-        for (UserHistory history : loadedUser.getHistory()) {
-            System.out.println(history.getEntryTime().toString() + " " + history.getEntry());
-        }
-        // Hibernate will do automatic update of User after changing using proxy
-        loadedUser.getProteinData().setTotal(loadedUser.getProteinData().getTotal() + 50);
-        loadedUser.addHistory(new UserHistory(new Date(), "Added 50 protein"));
-
-        user.setProteinData(new ProteinData());
-
-        session.getTransaction().commit();
-
-        session.close();
         HibernateUtilities.getSessionFactory().close();
+    }
+
+    private static void PopulateSampleData() {
+        Session session = HibernateUtilities.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        User joe = CreateUser("Joe", 500, 50, "Good job", "You made it!");
+        session.save(joe);
+
+        User bob = CreateUser("Bob", 300, 20, "Taco time!");
+        session.save(bob);
+
+        User amy = CreateUser("Amy", 250, 200, "Yes!!!");
+        session.save(amy);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    private static User CreateUser(String name, int goal, int total, String... alerts) {
+        User user = new User();
+        user.setName(name);
+        user.getProteinData().setGoal(goal);
+        user.addHistory(new UserHistory(new Date(), "Set goal to " + goal));
+        user.getProteinData().setTotal(total);
+        user.addHistory(new UserHistory(new Date(), "Set total to " + total));
+        for (String alert : alerts) {
+            user.getGoalAlerts().add(new GoalAlert(alert));
+        }
+
+        return user;
     }
 }
